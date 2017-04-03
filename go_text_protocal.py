206dbaf0d16f3f04ed0c420d00cdab0247519f16
ainu7 <ainu77@gmail.com>
2017-04-03 17:00:48 +0900

    Update go_text_protocal.py

--- go_text_protocal.py
+++ go_text_protocal.py
@@ -7,8 +7,27 @@ command_list = set(['clear_board', 'known_command', 'genmove', 'play', 'showboar
 turn = 1
 line = sys.stdin.readline()
 while line:
-  tokens = line.replace('\n', '').split(' ')
-  if tokens[0] == 'known_command':
+  tokens = line.replace('\n', '').lower().split(' ')
+  if tokens[0] == 'protocol_version':
+    print ('= 2')
+    print
+  elif tokens[0] == 'get_random_seed':
+    print('= 0')
+    print
+  elif tokens[0] == 'boardsize':
+    print('=')
+    print
+  elif tokens[0] == 'komi':
+    print('=')
+    print
+  elif tokens[0] == 'name':
+    print('= wonpago policy')
+    print
+  elif tokens[0] == 'version':
+    print('= 1')
+    print
+  elif tokens[0] == 'known_command':
+    print('=')
     print(command_list)
     print
   elif tokens[0] == 'clear_board':
@@ -43,9 +62,13 @@ while line:
     print('= %s%d' % (chr(ord('a') + int(pos / 10) - 1) , int(pos) % 10))
     print
   elif tokens[0] == 'showboard':
+    print('=')
     for l in s.show():
       print l
+    print
   else:
     print('?')
+
+  sys.stdout.flush()
 
   line = sys.stdin.readline()
