67d60d9573f6934b95b72915c42c7a91da76158b
ainu7 <ainu77@gmail.com>
2017-04-05 16:38:13 +0900

    Update go_text_protocal.py

--- go_text_protocal.py
+++ go_text_protocal.py
@@ -7,8 +7,27 @@ command_list = set(['clear_board', 'known_command', 'genmove', 'play', 'showboar
 turn = 1
 line = sys.stdin.readline()
 while line:
-  tokens = line.replace('\n', '').split(' ')
-  if tokens[0] == 'known_command':
+  tokens = line.replace('\n', '').lower().split(' ')
+  if tokens[0] == 'protocol_version':
+    print ('= 2')
+    print
+  elif tokens[0] == 'get_random_seed':
+    print('= 0')
+    print
+  elif tokens[0] == 'boardsize':
+    print('=')
+    print
+  elif tokens[0] == 'komi':
+    print('=')
+    print
+  elif tokens[0] == 'name':
+    print('= wonpago policy')
+    print
+  elif tokens[0] == 'version':
+    print('= 1')
+    print
+  elif tokens[0] == 'known_command':
+    print('=')
     print(command_list)
     print
   elif tokens[0] == 'clear_board':
@@ -21,7 +40,8 @@ while line:
         (tokens[1] == 'white' and turn == 1)):
       s.play(0)  # pass once to flip turn
       turn = 3 - turn
-    pos = (ord(tokens[2][0]) - ord('a') + 1) * 10 + int(tokens[2][1])
+    if tokens[2] != 'pass':
+      pos = (ord(tokens[2][0]) - ord('a') + 1) * 10 + int(tokens[2][1])
     s.play(pos)
     turn = 3 - turn
     print('=')
@@ -43,9 +63,13 @@ while line:
     print('= %s%d' % (chr(ord('a') + int(pos / 10) - 1) , int(pos) % 10))
     print
   elif tokens[0] == 'showboard':
+    print('=')
     for l in s.show():
       print l
+    print
   else:
     print('?')
+
+  sys.stdout.flush()
 
   line = sys.stdin.readline()
